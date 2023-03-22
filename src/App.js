dee9b6140b1573140ed421d70f45fa662463c117
fygure <92011471+fygure@users.noreply.github.com>
2023-03-22 13:43:50 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 
-function App() {
+const USER_TYPES = {
+  PUBLIC: 'Public User',
+  NORMAL_USER: 'Normal User',
+  ADMIN_USER: "Admin User",
+}
+
+
+const CURRENT_USER_TYPE = USER_TYPES.ADMIN_USER
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style= {{display:'flex', gap: 16, padding: 8}}>
+        <Link to={'/'}>Home</Link>
+        {(CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER || 
+          CURRENT_USER_TYPE === USER_TYPES.NORMAL_USER) ? 
+        <>
+          <Link Link to={'/user'}>User</Link>
+          <Link to={'/profile'}>Profile</Link>
+        </>
+        : null }
+
+        {(CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER) ? 
+        <>
+          <Link to={'/admin'}>Admin</Link>
+        </>
+        : null }
+        
+        
+        <div>You are logged in as: {CURRENT_USER_TYPE}</div>
+      </div>
+      <AppRoutes />
     </div>
-  );
+  )
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route 
+        path="/" 
+        element={
+          <PublicElement> 
+            <Home/> 
+          </PublicElement>
+        }
+      ></Route>
+      <Route
+        path="/user" 
+        element={
+          <UserElement>
+            <User />
+          </UserElement>
+      }
+      ></Route>
+      <Route
+        path="/profile" 
+        element={
+          <UserElement>
+            <User />
+          </UserElement>
+      }
+      ></Route>
+      <Route 
+        path="/admin" 
+        element={
+        <AdminElement>
+          <Admin />
+        </AdminElement>
+      }
+      ></Route>
+      <Route path="*" element={<>Page Not Found!</>}></Route>
+    </Routes>
+  )
+}
+
+function Home () {
+  return (
+    <div>
+      Home Page
+    </div>
+  )
+}
+
+function User () {
+  return (
+    <div>
+      User Page
+    </div>
+  )
+}
+
+function Admin () {
+  return (
+    <div>
+      Admin Page
+    </div>
+  )
+}
+
+function PublicElement ({children}) {
+  return (
+    <>{children}</>
+  )
+}
+
+function UserElement ({children}) {
+
+  if(CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER || 
+     CURRENT_USER_TYPE === USER_TYPES.NORMAL_USER)
+  {
+    return (
+      <>{children}</>
+    )
+  }
+  else
+  {
+    return (
+      <>You do not have access to this page.</>
+      //add back to home page button
+    )
+  }
+  
+}
+
+function AdminElement ({children}) {
+  if (CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER)
+  {
+    return (
+      <>{children}</>
+    )
+  }
+  else
+  {
+    return (
+      <>You do not have access to this page.</>
+      //add back to home page button
+    )
+  }
+  
 }
 
 export default App;
